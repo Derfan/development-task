e53d910597f5fa9954e9b4bc5b6f7df4787e232a
Boris the Blade <boris_blade@helpsyst.com>
2019-04-09 17:23:17 +0300

    add forgotten tests

---
+++ src/components/__tests__/Album.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {shallow} from "enzyme";
+import Album from "../Album";
+import {ListWrapper, StyledLink} from "../../helpers";
+import {album} from "./mock";
+
+const props = {
+    location: {state: {album}}
+};
+
+describe("Album", () => {
+    const component = shallow(<Album {...props} />);
+
+    it("render", () => {
+        expect(component.find(ListWrapper)).toHaveLength(1);
+        expect(component.find(StyledLink)).toHaveLength(props.location.state.album.length);
+    });
+
+    it("correct url", () => {
+        expect(component.find(StyledLink).first().prop("to")).toEqual({
+            pathname: `/${props.location.state.album[0].albumId}/${props.location.state.album[0].id}`,
+            state: {photo: props.location.state.album[0]}
+        });
+    });
+
+    it("correct title", () => {
+        expect(component.find(StyledLink).first().find("p").text()).toEqual(props.location.state.album[0].title)
+    });
+});
---
+++ src/components/__tests__/AlbumList.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {shallow} from "enzyme";
+import AlbumsList from "../AlbumsList";
+import {ListWrapper, StyledLink} from "../../helpers";
+import {albums} from "./mock";
+
+const props = {
+    albums
+};
+
+describe("Album", () => {
+    const component = shallow(<AlbumsList {...props} />);
+
+    it("render", () => {
+        expect(component.find(ListWrapper)).toHaveLength(1);
+        expect(component.find(StyledLink)).toHaveLength(props.albums.length);
+    });
+
+    it("correct url", () => {
+        expect(component.find(StyledLink).first().prop("to")).toEqual({
+            pathname: `/${props.albums[0][0].albumId}`,
+            state: {album: props.albums[0]}
+        });
+    });
+
+    it("correct title", () => {
+        expect(component.find(StyledLink).first().find("p").text()).toEqual(`Album ${props.albums[0][0].albumId}`);
+    });
+});
